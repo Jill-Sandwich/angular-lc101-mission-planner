e7ab6c883b6adee6593afd532a960068d87f4f9e
Katie Dessain <JillSandwich@example.com>
2021-08-07 19:31:33 -0500

    updated code to edit crew members

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
@@ -26,5 +28,14 @@ export class CrewComponent implements OnInit {
     let index = this.crew.indexOf(member);
     this.crew.splice(index, 1);
   }
+
+  edit(member: object){
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+    }
 
 }
