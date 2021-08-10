06f99e5b94ab4b6c9a3cffe3da3ea47da1af1c71
Katie Dessain <JillSandwich@example.com>
2021-08-09 21:56:27 -0500

    updated experiments component to be editable for the bonus mission

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experimentBeingEdited: string = null;
+
+  experimentSamples: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(experimentItem: string) {
+    this.experimentSamples.push(experimentItem);
+  }
+  
+  remove(experimentItem: string) {
+    let index = this.experimentSamples.indexOf(experimentItem);
+    this.experimentSamples.splice(index, 1);
+  }
+
+  edit(experimentItem: string) {
+    this.experimentBeingEdited = experimentItem;
+ }
+
+  save(oldExperimentItem: string, newExperimentItem: string) {
+  let index = this.experimentSamples.indexOf(oldExperimentItem);
+  this.experimentSamples[index] = newExperimentItem;
+  this.experimentBeingEdited = null;
   }
 
 }
