ed350acaf99023cb5f64e09992268f87d475818b
Katie Dessain <JillSandwich@example.com>
2021-08-09 21:18:42 -0500

    updated equipment component to be editable for the bonus mission

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipmentBeingEdited: string = null;
+
+  equipmentItems: string[] = ['Habitat dome','Drones', 'Food containers','Oxygen tanks'];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(equipmentItem: string) {
+    this.equipmentItems.push(equipmentItem);
+  }
+  
+  remove(equipmentItem: string) {
+    let index = this.equipmentItems.indexOf(equipmentItem);
+    this.equipmentItems.splice(index, 1);
+  }
+
+  edit(equipmentItem: string) {
+    this.equipmentBeingEdited = equipmentItem;
+ }
+
+  save(oldEquipmentItem: string, newEquipmentItem: string) {
+  let index = this.equipmentItems.indexOf(oldEquipmentItem);
+  this.equipmentItems[index] = newEquipmentItem;
+  this.equipmentBeingEdited = null;
+  }
+
+ /*save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }*/
 
 }
